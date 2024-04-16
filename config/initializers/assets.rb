7da212b38dc4709e18bd1257473019a459dfc9bf
Lisha Ranganath <lishar2002@gmail.com>
2024-04-16 07:05:55 +0530

    assets precompile error- corrected

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompie += %w(blog_engine/application.css blog_engine/application.js)
+Rails.application.config.assets.precompile += %w(blog_engine/application.css blog_engine/application.js)
